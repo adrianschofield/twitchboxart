1a99f91b451186cf99f9fba8de9e58cff988186e
Adrian Schofield <adrianschofield@hotmail.co.uk>
2022-06-30 19:24:45 +0100

    Removing unecessary variable definition

--- scripts/variables-sample.js
+++ scripts/variables-sample.js
@@ -1,7 +1,6 @@
 'use strict';
 
 // Update these values to reflect your specific oauth configuration
-var channel = 'unshapedadrian';
 var clientId = 'client id here';
 var oAuthToken = 'oauth token here';
 var refreshToken = "refresh token here"
